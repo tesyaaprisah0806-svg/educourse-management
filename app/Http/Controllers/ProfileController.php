c1618aa540d2061960b7dc19921a3de7930f12e0
Tesya Aprisah <tesyaaprisah0806@gmail.com>
2026-05-12 11:57:39 +0700

    fix:add missing jurusan key in profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,22 +1,10 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class ProfileController extends Controller
+public function index()
 {
-    public function index()
-    {
-        $profile = [
-            'nama' => 'Tesya Aprisah', 
-            'nim'  => '25781024',          
-            'kelas' => 'MI 2A',
-            'skills' => [
-                ['name' => 'Laravel', 'level' => 85, 'color' => 'bg-danger'],
-                ['name' => 'PHP', 'level' => 80, 'color' => 'bg-primary'],
-                ['name' => 'Bootstrap', 'level' => 90, 'color' => 'bg-info'],
-                ['name' => 'JavaScript', 'level' => 75, 'color' => 'bg-warning'],
-            ]
-        ];
-        return view('profile.index', compact('profile'));
-    }
+    $profile = [
+        'nama' => 'Tesya Aprisah',
+        'nim' => '25781024',
+        'jurusan' => 'Manajemen Informatika', // Pastikan baris ini ada
+    ];
+
+    return view('profile.index', compact('profile'));
 }
